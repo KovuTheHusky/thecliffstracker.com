c71399756705a92b4ffdd6347b44813fd4e0a498
KovuTheHusky <kovuthehusky@gmail.com>
2021-02-09 19:26:13 -0500

    Add pagination for averages

--- average.php
+++ average.php
@@ -63,6 +63,17 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/cliffs-tracker/includes/header.php');
 ?>
 
 <nav aria-label="..." style="margin-top: 16px;">
+  <ul class="pagination justify-content-center">
+    <li class="page-item<?php if ($weekday == 0) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/0">U</a></li>
+    <li class="page-item<?php if ($weekday == 1) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/1">M</a></li>
+    <li class="page-item<?php if ($weekday == 2) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/2">T</a></li>
+    <li class="page-item<?php if ($weekday == 3) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/3">W</a></li>
+    <li class="page-item<?php if ($weekday == 4) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/4">R</a></li>
+    <li class="page-item<?php if ($weekday == 5) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/5">F</a></li>
+    <li class="page-item<?php if ($weekday == 6) { echo ' active'; } ?>"><a class="page-link" href="/cliffs-tracker/average/6">S</a></li>
+  </ul>
+</nav>
+
   <div style="width: 100%; height: calc(100% - 126px);">
     <canvas id="chart"></canvas>
   </div>
